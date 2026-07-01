bb3c6560954930eec425680aaaba4fd4d8d15a6c
Tarun Tomar <tt8022641@gmail.com>
2026-07-01 17:00:45 +0530

    Update flask_backend.py

--- flask_backend.py
+++ flask_backend.py
@@ -269,5 +269,9 @@ def retrieve():
         return jsonify({"error": str(exc)}), 500
 
 
+
+    import os
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
